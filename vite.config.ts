0d13413eaf0227a3e95175ddf3c5ffbb67a2a2b5
ChaitanyaAdepu <adepuchaitanya2@gmail.com>
2026-09-14 22:57:36 +0530

    changes for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Riddhi-Siddhi-Jewellers/',
   plugins: [react()],
   resolve: {
     alias: {
